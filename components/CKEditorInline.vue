bbbe65e0af2f07f30902563627599e378c2a30c1
supattra pangjun <supattra.pa@kkumail.com>
2024-04-25 17:08:23 +0700

    first commit

---
+++ components/CKEditorInline.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      <ckeditor :editor="editor" v-model="editorData" :config="editorConfig"></ckeditor>
+    </div>
+  </template>
+  
+  <script>
+  
+  import CKEditor from '@ckeditor/ckeditor5-vue';
+  import InlineEditor from '@ckeditor/ckeditor5-build-inline';
+  
+  export default {
+    components: {
+      ckeditor: CKEditor.component,
+    },
+    data() {
+      return {
+        editor: InlineEditor,
+        editorData: '',
+        editorConfig: {
+          // Configuration options
+        },
+      };
+    },
+  };
+  </script>
+  
---
+++ components/ui-components/cards/CardsProps.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ref, onMounted, onBeforeUnmount } from 'vue';
+import EditorJS from '@editorjs/editorjs';
+
+const editor = ref<EditorJS | null>(null);
+
+onMounted(() => {
+  editor.value = new EditorJS({
+    holder: 'editor',
+  });
+});
+
+onBeforeUnmount(() => {
+  if (editor.value) {
+    editor.value.destroy();
+  }
+});
+</script>
+
+<template>
+  <div>
+    <v-card elevation="0" title="This is a title" subtitle="This is a subtitle" text="This is content">
+      <div id="editor" class="editor"></div>
+    </v-card>
+  </div>
+</template>
