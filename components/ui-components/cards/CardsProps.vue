06738c7cd07c21878793abacb7fbbe5111dc788b
supattra pangjun <supattra.pa@kkumail.com>
2024-04-26 09:35:42 +0700

    commit show out put in json

--- components/ui-components/cards/CardsProps.vue
+++ components/ui-components/cards/CardsProps.vue
@@ -3,6 +3,7 @@ import { ref, onMounted, onBeforeUnmount } from 'vue';
 import EditorJS from '@editorjs/editorjs';
 
 const editor = ref<EditorJS | null>(null);
+const editorContent = ref<any>(null); // Use appropriate type for the content
 
 onMounted(() => {
   editor.value = new EditorJS({
@@ -15,6 +16,15 @@ onBeforeUnmount(() => {
     editor.value.destroy();
   }
 });
+
+const getContent = () => {
+  if (editor.value) {
+    editor.value.save().then((outputData: any) => {
+      editorContent.value = outputData;
+    })
+  }
+}
+
 </script>
 
 <template>
@@ -22,5 +32,14 @@ onBeforeUnmount(() => {
     <v-card elevation="0" title="This is a title" subtitle="This is a subtitle" text="This is content">
       <div id="editor" class="editor"></div>
     </v-card>
+    <v-btn @click="getContent">Show Output</v-btn>
+
+    <div v-if="editorContent">
+      <pre>{{ editorContent }}</pre>
+    </div>
+
+    <div>
+      <!-- {{ editorContent.blocks.data.text }} -->
+    </div>
   </div>
 </template>
