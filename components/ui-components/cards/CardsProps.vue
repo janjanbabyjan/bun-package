7dbcc70a9cb5f0ac9331694124f7ce8017f9721f
supattra pangjun <supattra.pa@kkumail.com>
2024-04-29 10:38:50 +0700

    commit list

--- components/ui-components/cards/CardsProps.vue
+++ components/ui-components/cards/CardsProps.vue
@@ -3,12 +3,8 @@ import { ref, onMounted, onBeforeUnmount } from 'vue';
 import EditorJS from '@editorjs/editorjs';
 import Header from '@editorjs/header';
 import LinkTool from '@editorjs/link';
-<<<<<<< HEAD
 import edjsHTML from 'editorjs-html';
-=======
-import Embed from '@editorjs/embed';
-
->>>>>>> b4a6cf05c1bb990d570cae0ead65b9b66bea9b59
+import List from '@editorjs/list';
 
 const editor = ref<EditorJS | null>(null);
 const editorContent = ref<any>(null);
@@ -20,26 +16,20 @@ onMounted(() => {
     holder: 'editor',
     tools: {
       header: Header,
+      list: {
+        class: List,
+        inlineToolbar: true,
+        config: {
+          defaultStyle: 'unordered'
+        }
+      },
       linkTool: {
         class: LinkTool,
         config: {
-<<<<<<< HEAD
-          endpoint: 'http://localhost:3000',
-        }
-=======
           endpoint: '',
         }
       },
-      embed: {
-        class: Embed,
-        config: {
-          services: {
-            youtube: true,
-            coub: true
-          }
-        }
->>>>>>> b4a6cf05c1bb990d570cae0ead65b9b66bea9b59
-      }
+
     },
   });
 });
@@ -74,19 +64,7 @@ const displayHTML = (html: string) => {
       <div id="editor" class="editor" style="padding: 1rem;"></div>
     </v-card>
     <v-btn @click="getContent">Show Output</v-btn>
-<<<<<<< HEAD
     <br>
     <div id="output-container"></div> <!-- Display HTML output here -->
-=======
-    <div v-if="editorContent">
-      <br>
-      <br>
-      <pre>{{ editorContent.blocks.map((block: any) => block.data.text).join('\n') }}</pre>
-      <hr>
-      <pre>{{ editorContent }}</pre>
-
-    </div>
-
->>>>>>> b4a6cf05c1bb990d570cae0ead65b9b66bea9b59
   </div>
 </template>
