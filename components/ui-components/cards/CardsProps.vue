603fdb77479c2546ececad99fc4e03519ea7c01a
kazetaw <Thanchira.m@kkumail.com>
2024-04-29 10:41:57 +0700

    show output html latest

--- components/ui-components/cards/CardsProps.vue
+++ components/ui-components/cards/CardsProps.vue
@@ -3,17 +3,11 @@ import { ref, onMounted, onBeforeUnmount } from 'vue';
 import EditorJS from '@editorjs/editorjs';
 import Header from '@editorjs/header';
 import LinkTool from '@editorjs/link';
-<<<<<<< HEAD
 import edjsHTML from 'editorjs-html';
-=======
-import Embed from '@editorjs/embed';
-
->>>>>>> b4a6cf05c1bb990d570cae0ead65b9b66bea9b59
 
 const editor = ref<EditorJS | null>(null);
 const editorContent = ref<any>(null);
 const edjsParser = edjsHTML(); // Initialize editorjs-html
-
 
 onMounted(() => {
   editor.value = new EditorJS({
@@ -23,22 +17,8 @@ onMounted(() => {
       linkTool: {
         class: LinkTool,
         config: {
-<<<<<<< HEAD
           endpoint: 'http://localhost:3000',
         }
-=======
-          endpoint: '',
-        }
-      },
-      embed: {
-        class: Embed,
-        config: {
-          services: {
-            youtube: true,
-            coub: true
-          }
-        }
->>>>>>> b4a6cf05c1bb990d570cae0ead65b9b66bea9b59
       }
     },
   });
@@ -74,19 +54,7 @@ const displayHTML = (html: string) => {
       <div id="editor" class="editor" style="padding: 1rem;"></div>
     </v-card>
     <v-btn @click="getContent">Show Output</v-btn>
-<<<<<<< HEAD
     <br>
     <div id="output-container"></div> <!-- Display HTML output here -->
-=======
-    <div v-if="editorContent">
-      <br>
-      <br>
-      <pre>{{ editorContent.blocks.map((block: any) => block.data.text).join('\n') }}</pre>
-      <hr>
-      <pre>{{ editorContent }}</pre>
-
-    </div>
-
->>>>>>> b4a6cf05c1bb990d570cae0ead65b9b66bea9b59
   </div>
 </template>
